835ccb33296968bff8205f40b2a8e6ff46e655d9
Olivier Houyoux <olivier.houyoux@gmail.com>
2015-12-11 22:20:38 +0100

    Make the example simple enough

--- src/main/java/be/nitroxis/functor/Main.java
+++ src/main/java/be/nitroxis/functor/Main.java
@@ -2,7 +2,7 @@ package be.nitroxis.functor;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.StringReader;
 
 /**
  * Shows the benefits of using the {@code Throwers} utility class with a simple example.
@@ -35,7 +35,7 @@ public class Main {
      */
     
     public static void main(final String[] args) {
-        Thrower<String, IOException> readLine = () -> new BufferedReader(new InputStreamReader(System.in)).readLine();
+        Thrower<String, IOException> readLine = () -> new BufferedReader(new StringReader("Hello world")).readLine();
         UnaryFunction<Thrower<String, IOException>, Thrower<Integer, IOException>> f = Throwers.map(length);
         System.out.println(ExceptionHandler.evaluate(f.evaluate(readLine)));
     }
