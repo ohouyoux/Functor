d843e080572b4539fca79676944b3009bff61b23
Olivier Houyoux <olivier.houyoux@gmail.com>
2015-12-11 19:23:01 +0100

    Initial commit

---
+++ src/main/java/be/nitroxis/functor/Functor.java
@@ -0,0 +1,11 @@
+package be.nitroxis.functor;
+
+/**
+ * A marker interface for Functors.
+ * 
+ * @author Olivier Houyoux (olivier.houyoux@gmail.com)
+ * @see https://en.wikipedia.org/wiki/Function_object
+ */
+public interface Functor {
+
+}
